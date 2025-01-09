f929ed22c704f5f02f6570e4dc760f17d3328b25
community-vercel <zuhooruddin055#gmail.com>
2025-01-09 04:10:26 -0800

    commited

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -6,26 +6,40 @@ import 'react-toastify/dist/ReactToastify.css';  // Import CSS for toastify
 
 function ContactForm() {
   const [result, showresult] = useState(false);
+  const serverurls = process.env.NEXT_PUBLIC_DJANGO_URLS;
+
   const formRef = useRef();
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm("service_nngmacg", "template_bz9p9la", formRef.current, {
-        publicKey: "kyRFMbNyw6IsVBzgb",
-      })
-      .then(
-        (result) => {
-          showresult(true);
-          toast.success("Your Message has been successfully sent."); // Show success toast
-        },
-        (error) => {
-          console.log(error.text);
-          toast.error("An error occurred while sending your message. Please try again later."); // Show error toast
-        }
-      );
+    const formData = new FormData(formRef.current);
 
-    e.target.reset();
+    const data = {
+      from_name: formData.get("from_name"),
+      from_email: formData.get("from_email"),
+      from_phone: formData.get("from_phone"),
+      from_subject: formData.get("from_subject"),
+      message: formData.get("message"),
+    };
+
+    try {
+      const response = await fetch(`${serverurls}add-contactss/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        toast.success("Thank you for contacting with us ")
+        formRef.current.reset();
+      } else {
+        toast.error("not sumitte sucessfully")
+        const errorData = await response.json();
+      }
+    } catch (error) {
+    }
   };
 
   return (
